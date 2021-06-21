d59974be033ca3eb9dd53023e701f8b5c9ece595
Tiberiu Iulian Dafinescu <lordiulian@gmail.com>
2021-06-21 22:32:18 +0200

    pagination

--- src/Controller/Ranking/PlayersController.php
+++ src/Controller/Ranking/PlayersController.php
@@ -5,7 +5,9 @@ namespace App\Controller\Ranking;
 use App\Entity\Player;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * @Route("/{_locale}/ranking")
@@ -15,7 +17,7 @@ class PlayersController extends AbstractController
     /**
      * @Route("/players", name="ranking_players")
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
         // return the "player" db entity manager
         $em = $this->getDoctrine()->getManager('player');
@@ -23,9 +25,16 @@ class PlayersController extends AbstractController
         // return all the players
         $players = $em->getRepository(Player::class)->findAll();
 
+        // list players in different pages  
+        $pagination = $paginator->paginate(
+            $players, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            20 /*limit per page*/
+        );
+
         return $this->render('ranking/players/index.html.twig', [
             'controller_name' => 'PlayersController',
-            'players' => $players
+            'players' => $pagination
         ]);
     }
 }
